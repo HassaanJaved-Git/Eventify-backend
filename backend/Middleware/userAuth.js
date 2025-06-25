160171723f54fe1189e2a3337ea8c7a5c557e48e
H M <hasanmunir406@gmail.com>
2025-06-25 16:26:12 +0500

    Updated

--- backend/Middleware/userAuth.js
+++ backend/Middleware/userAuth.js
@@ -6,7 +6,6 @@ const userSecretKEY = process.env.JWTuserSecretKEY;
 
 const authenticateUser = (req, res, next) => {
     const authHeader = req.headers['authorization'];
-    console.log("authHeader:", authHeader);
 
     if (!authHeader || !authHeader.startsWith('Bearer ')) return res.status(401).json({ error: 'No token provided or malformed' });
 
